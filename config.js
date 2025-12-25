faaa0fae5c6065b5d1d00bc429ac7ef00f95a0b0
Venards <113711527+Venards@users.noreply.github.com>
2025-12-25 14:39:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 const firebaseConfig = {
     apiKey: "AIzaSyCdlefibbnAvbdZ86lPJAdUtifGPP7uQTE",
     authDomain: "tts-projet-fd867.firebaseapp.com",
-    databaseURL: "https://tts-projet-fd867-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    databaseURL: "https://tts-projet-fd867-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "tts-projet-fd867",
     storageBucket: "tts-projet-fd867.firebasestorage.app",
     messagingSenderId: "412421975876",
-    appId: "1:412421975876:web:bae419fa68b429ba52f111"
+    appId: "1:412421975876:web:e32be7f582c545bf52f111"
 };
 
 // Initialize Firebase
@@ -16,3 +16,4 @@ try {
 } catch (error) {
     console.error('Firebase initialization error:', error);
 }
+
